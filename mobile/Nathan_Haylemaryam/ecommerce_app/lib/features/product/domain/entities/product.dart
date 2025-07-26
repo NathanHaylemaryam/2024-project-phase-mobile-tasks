77046d687d7961912835ace7c58ece090fbd1091
Nathan Haylemaryam <nathanhaylemaryam482@gmail.com>
2025-07-26 22:00:13 +0300

    Revert "task 9"
    This reverts commit 979c4329634d215daf858f030d2f7606d8f7725a.

--- mobile/Nathan_Haylemaryam/ecommerce_app/lib/features/product/domain/entities/product.dart
+++ mobile/Nathan_Haylemaryam/ecommerce_app/lib/features/product/domain/entities/product.dart
@@ -1,20 +1,23 @@
-import 'package:equatable/equatable.dart';
-
-class Product extends Equatable {
-  final String id;
+class Product {
   final String name;
-  final String description;
-  final double price;
-  final String imageUrl;
+  final String decscription;
+  final String price;
 
-  const Product({
-    required this.id,
+  final String imagepath;
+
+  final int id;
+
+
+
+  Product(
+  {
     required this.name,
-    required this.description,
+    required this.decscription,
     required this.price,
-    required this.imageUrl,
-  });
+    required this.imagepath,
+    required this.id,
 
-  @override
-  List<Object> get props => [id, name, description, price, imageUrl];
+
+}
+      );
 }
--- mobile/Nathan_Haylemaryam/ecommerce_app/lib/features/product/domain/repository/product_repository.dart
+++ mobile/Nathan_Haylemaryam/ecommerce_app/lib/features/product/domain/repository/product_repository.dart
@@ -1,10 +1,10 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/error/failure.dart';
-import '../entities/product.dart';
+import '../../../../utils/product.dart';
 
 abstract class ProductRepository {
-  Future<Either<Failure, void>> insertProduct(Product product);
-  Future<Either<Failure, void>> updateProduct(Product product);
-  Future<Either<Failure, void>> deleteProduct(String id);
-  Future<Either<Failure, Product>> getProduct(String id);
+  Future<List<Product>> getAllProducts();
+  Future<Product> getProductById(String name);
+  Future<void> createProduct(Product product);
+  Future<void> updateProduct(Product product);
+  Future<void> deleteProduct(String id);
+
 }
