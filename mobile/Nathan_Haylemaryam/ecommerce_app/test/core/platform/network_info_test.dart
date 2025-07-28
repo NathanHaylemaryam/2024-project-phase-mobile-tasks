e87d2ab5cc701a5284707a471fbf675378728045
Nathan Haylemaryam <nathanhaylemaryam482@gmail.com>
2025-07-28 16:50:47 +0300

    task 13

---
+++ mobile/Nathan_Haylemaryam/ecommerce_app/test/core/platform/network_info_test.dart
@@ -0,0 +1,38 @@
+// test/core/platform/network_info_test.dart
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:ecommerce_app/core/platform/network_info.dart';
+
+class MockInternetConnectionChecker extends Mock implements InternetConnectionChecker {
+  @override
+  Future<bool> get hasConnection => super.noSuchMethod(
+    Invocation.getter(#hasConnection),
+    returnValue: Future.value(true), // default value
+    returnValueForMissingStub: Future.value(true),
+  );
+}
+
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockConnectionChecker;
+
+  setUp(() {
+    mockConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockConnectionChecker);
+  });
+
+  test('isConnected should forward the call to InternetConnectionChecker.hasConnection',
+        () async {
+      // arrange
+      when(mockConnectionChecker.hasConnection).thenAnswer((_) async => true);
+
+      // act
+      final result = await networkInfo.isConnected;
+
+      // assert
+      verify(mockConnectionChecker.hasConnection);
+      expect(result, true);
+    },
+  );
+}
